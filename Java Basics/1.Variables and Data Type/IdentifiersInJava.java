f7399ea9f7a8ab3b097f4ac5a9abe25e92425745
Raunak Raj <rounaks4569@gmail.com>
2023-09-16 22:43:45 +0530

    Identifiers In Java

---
+++ Java Basics/1.Variables and Data Type/IdentifiersInJava.java
@@ -0,0 +1,19 @@
+public class IdentifiersInJava {
+    public static void main(String[] args) {
+        final int var1 = 99;
+        double var2 = 2.0;
+        System.out.println("Hello World!");
+        // Identifiers List in this program
+
+        // 1.IdentifiersInJava (Class name)
+        // 2.main (Method name)
+        // 3.String (Predefined Class name)
+        // 4.args (String variable name)
+        // 5.var1 (integer variable name)
+        // 6.var2 (double variable name)
+        // 7.System(Predefined Class name)
+        // 8.out(Variable name)
+        // 9.println (Method name)
+    }
+    
+}
