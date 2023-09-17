5fd43caa3d8550c8c31a0e39d6e88ca4acccc097
Raunak Raj <rounaks4569@gmail.com>
2023-09-17 08:31:22 +0530

    Left hand half pyramid

---
+++ Java Basics/7.Patterns Print/LeftHalfPyramid.java
@@ -0,0 +1,24 @@
+public class LeftHalfPyramid {
+    public static void main(String[] args) {
+        /*
+    
+         * 
+         * *
+         * * *
+         * * * *
+         * * * * *
+        
+         */
+        int n =5;
+        int i = 1;
+        for (i =1; i<=n;i++) {
+            int j =1;
+            for(j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+            
+        }
+    }
+    
+}
