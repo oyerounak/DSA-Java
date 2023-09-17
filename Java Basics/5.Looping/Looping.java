914b16bcb3031d32f860720a8c7594b50297169a
Raunak Raj <rounaks4569@gmail.com>
2023-09-17 08:25:53 +0530

    Looping in java

---
+++ Java Basics/5.Looping/Looping.java
@@ -0,0 +1,32 @@
+
+public class Looping {
+    public static void main(String[] args) {
+
+        //for loop
+        int i=0;
+        for(;i<5 ;i+=2){
+            System.out.println("ShwRa");
+        }
+        System.out.println(i); //output : 6
+
+        //while loop
+        int j=0;
+        while(j<5){
+            System.out.println("Raunak");
+            j+=2;
+        }
+        System.out.println(j); //output : 6
+
+        //Do-While
+        int k =0;
+        do{
+            System.out.println("Me");
+            k++;
+        }while(k>5);
+
+        //
+
+        
+    }
+    
+}
